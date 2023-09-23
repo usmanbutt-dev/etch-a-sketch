8a66087a7304448ec326ffdcc6c5981481ce5a26
Usman <buttu7666@gmail.com>
2023-09-23 18:25:47 +0500

    Click and dragpaint logic completed

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let gridBoxes = document.querySelectorAll(".grid-boxes");
-console.log(gridBoxes);
+let isMouseDown = false;
 
 gridBoxes.forEach(box => {
-    box.addEventListener("mousemove", () => {
+    box.addEventListener("mousedown", () => {
+        isMouseDown = true;
         box.style.backgroundColor = "brown";
+        console.log(isMouseDown);
     })
 });
+
+gridBoxes.forEach(box => {
+    box.addEventListener("mouseover", () => {
+        if(isMouseDown) {
+            box.style.backgroundColor = "brown";
+            console.log(isMouseDown);
+        }
+    })
+})
+
+document.addEventListener("mouseup", ()=> {
+    isMouseDown = false;
+    console.log(isMouseDown);
+}) 
