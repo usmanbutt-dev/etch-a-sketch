35c21fd37ba99dc35b1370dac3b5ce7e6e6cd0dd
Usman <buttu7666@gmail.com>
2023-09-24 03:25:41 +0500

    Grid size setter created, no functionality yet

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let gridBoxes = document.querySelectorAll(".grid-boxes");
+let colorChooser = document.querySelector("#chosen-color");
+let gridSizer = document.querySelector("#grid-sizer");
+let chosenSize = document.querySelector("#chosen-size");
+let chosenColor = "black";
 let isMouseDown = false;
+
+colorChooser.addEventListener(`input`, () => {
+    chosenColor = colorChooser.value;
+})
+
+gridSizer.addEventListener(`input`, () => {
+    console.log(chosenSize);
+    chosenSize.innerHTML = gridSizer.value + " x " + gridSizer.value;
+    console.log(chosenSize);
+})
+
 
 gridBoxes.forEach(box => {
     box.addEventListener("mousedown", () => {
         isMouseDown = true;
-        box.style.backgroundColor = "brown";
+        box.style.backgroundColor = chosenColor;
         console.log(isMouseDown);
     })
 });
@@ -12,7 +27,7 @@ gridBoxes.forEach(box => {
 gridBoxes.forEach(box => {
     box.addEventListener("mouseover", () => {
         if(isMouseDown) {
-            box.style.backgroundColor = "brown";
+            box.style.backgroundColor = chosenColor;
             console.log(isMouseDown);
         }
     })
@@ -21,4 +36,4 @@ gridBoxes.forEach(box => {
 document.addEventListener("mouseup", ()=> {
     isMouseDown = false;
     console.log(isMouseDown);
-}) 
+})
